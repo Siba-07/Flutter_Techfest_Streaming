cc642f574f921fa28f67fd73149b5c9fa260500e
pranav viswanatha <pranav279.v@gmail.com>
2020-08-13 19:17:33 +0530

    json in comments

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -4,30 +4,27 @@ import 'package:flutter/material.dart';
 class VideoInfo {
   String videoUrl;
   String videoTitle;
-  String videoAuthor;
   String imageUrl;
   String description;
 
   VideoInfo({
     this.videoUrl,
-    this.videoTitle, 
-    this.videoAuthor,
+    this.videoTitle,
     this.imageUrl,
     this.description
     });
 
-  factory VideoInfo.fromJson(dynamic json) {
+  /*factory VideoInfo.fromJson(dynamic json) {
     return VideoInfo(
       videoAuthor: json['videoAuthor'] as String,
-      videoTitle: json['videoTitle'] as String,
       videoUrl: json['videoUrl'] as String,
       imageUrl: json['imageUrl'] as String,
       description: json['description'] as String,
     );
-  }
+  }*/
 }
 
-class Tutorial {
+/*class Tutorial {
 
   List <VideoInfo> tags;
 
@@ -46,37 +43,31 @@ class Tutorial {
       return Tutorial();
     }
   }
-}
-
-final List<VideoInfo> movies=
+}*/
 
 final List<VideoInfo> videos = [
   VideoInfo(
-    videoUrl:'https://www.youtube.com/watch?v=J7pJSnDCnYI',
+    videoUrl:'J7pJSnDCnYI',
     videoTitle:'Video 1',
-    videoAuthor:'Techfest IIT Bombay',
-    imageUrl:'assets/images/nutcracker.jpg',
+    imageUrl:'http://i3.ytimg.com/vi/J7pJSnDCnYI/hqdefault.jpg',
     description: 'dfghjk',
   ),
   VideoInfo(
-    videoUrl:'https://www.youtube.com/watch?v=0_FBwJi8VBo',
+    videoUrl:'0_FBwJi8VBo',
     videoTitle:'Video 2',
-    videoAuthor:'Techfest IIT Bombay',
-    imageUrl:'assets/images/spiderman.jpg',
+    imageUrl:'http://i3.ytimg.com/vi/0_FBwJi8VBo/hqdefault.jpg',
     description: 'dfghjk',
   ),
   VideoInfo(
-    videoUrl:'https://www.youtube.com/watch?v=2z3CEBv8PLI',
+    videoUrl:'2z3CEBv8PLI',
     videoTitle:'Video 3',
-    videoAuthor:'Techfest IIT Bombay',
-    imageUrl:'assets/images/toystory.jpg',
+    imageUrl:'http://i3.ytimg.com/vi/2z3CEBv8PLI/hqdefault.jpg',
     description: 'dfghjk',
   ),
   VideoInfo(
-    videoUrl:'https://www.youtube.com/watch?v=J8XJjkA5NuQ',
+    videoUrl:'J8XJjkA5NuQ',
     videoTitle:'Video 4',
-    videoAuthor:'Techfest IIT Bombay',
-    imageUrl:'assets/images/spiderman_1.jpg',
+    imageUrl:'http://i3.ytimg.com/vi/J8XJjkA5NuQ/hqdefault.jpg',
     description: 'dfghjk',
   ),
 
